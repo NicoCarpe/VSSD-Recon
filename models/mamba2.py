2677ea60dc447bcad06787925d195e183cdfbd50
NicoCarpe <ngcarpen@ualberta.ca>
2025-03-10 13:34:24 -0700

    streamlined mamba2 implemenation.

--- models/mamba2.py
+++ models/mamba2.py
@@ -1,31 +1,28 @@
 # Copyright (c) 2024, Tri Dao, Albert Gu.
 
 import math
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from einops import rearrange
+from einops import rearrange, repeat
 
 from mamba_ssm.ops.triton.layernorm_gated import RMSNorm as RMSNormGated
 from mamba_ssm.distributed.tensor_parallel import ColumnParallelLinear, RowParallelLinear
 from mamba_ssm.distributed.distributed_utils import all_reduce, reduce_scatter
 from mamba_ssm.ops.triton.ssd_combined import mamba_chunk_scan_combined, mamba_split_conv1d_scan_combined
 
-from huggingface_hub import PyTorchModelHubMixin
 
 
-class Mamba2(nn.Module, PyTorchModelHubMixin):
+class Mamba2(nn.Module):
     def __init__(
         self,
         d_model,
-        d_state=128,
+        d_state=64,
         d_conv=4,
         conv_init=None,
         expand=2,
-        headdim=64,
-        d_ssm=None,  # If not None, we only apply SSM on this many dimensions, the rest uses gated MLP
+        headdim=128,
         ngroups=1,
         A_init_range=(1, 16),
         D_has_hdim=False,
@@ -34,6 +31,8 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         dt_min=0.001,
         dt_max=0.1,
         dt_init_floor=1e-4,
+        dt_limit=(0.0, float("inf")),
+        activation="swish",
         bias=False,
         conv_bias=True,
         # Fused kernel and sharding options
@@ -41,6 +40,7 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         use_mem_eff_path=True,
         process_group=None,
         sequence_parallel=True,
+        layer_idx=None,  # Absorb kwarg for general module
         device=None,
         dtype=None,
     ):
@@ -51,24 +51,26 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         self.d_conv = d_conv
         self.conv_init = conv_init
         self.expand = expand
+        #======================================================================
         self.process_group = process_group
         self.sequence_parallel = sequence_parallel
         self.world_size = 1 if process_group is None else process_group.size()
         self.local_rank = 0 if process_group is None else process_group.rank()
-        self.d_inner = (self.expand * self.d_model) // self.world_size
+        #======================================================================
         assert self.d_inner * self.world_size == self.expand * self.d_model
+        self.d_inner = (self.expand * self.d_model) // self.world_size
         self.headdim = headdim
-        self.d_ssm = self.d_inner if d_ssm is None else d_ssm // self.world_size
         assert ngroups % self.world_size == 0
         self.ngroups = ngroups // self.world_size
-        assert self.d_ssm % self.headdim == 0
-        self.nheads = self.d_ssm // self.headdim
+        assert self.d_inner % self.headdim == 0
+        self.nheads = self.d_inner // self.headdim
         self.D_has_hdim = D_has_hdim
         self.rmsnorm = rmsnorm
-        self.norm_before_gate = norm_before_gate
-        self.activation = "silu"
+        self.dt_limit = dt_limit
+        self.activation = activation
         self.chunk_size = chunk_size
         self.use_mem_eff_path = use_mem_eff_path
+        self.layer_idx = layer_idx
 
         # Order: [z, x, B, C, dt]
         d_in_proj = 2 * self.d_inner + 2 * self.ngroups * self.d_state + self.nheads
@@ -79,7 +81,7 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
                                                 process_group=self.process_group, sequence_parallel=self.sequence_parallel,
                                                 **factory_kwargs)
 
-        conv_dim = self.d_ssm + 2 * self.ngroups * self.d_state
+        conv_dim = self.d_inner + 2 * self.ngroups * self.d_state
         self.conv1d = nn.Conv1d(
             in_channels=conv_dim,
             out_channels=conv_dim,
@@ -91,6 +93,7 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         )
         if self.conv_init is not None:
             nn.init.uniform_(self.conv1d.weight, -self.conv_init, self.conv_init)
+        # self.conv1d.weight._no_weight_decay = True
 
         self.act = nn.SiLU()
 
@@ -107,20 +110,28 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         # name.endswith("bias") in param_grouping.py
         self.dt_bias._no_weight_decay = True
 
+        # A parameter
         assert A_init_range[0] > 0 and A_init_range[1] >= A_init_range[0]
         A = torch.empty(self.nheads, dtype=torch.float32, device=device).uniform_(*A_init_range)
         A_log = torch.log(A).to(dtype=dtype)
         self.A_log = nn.Parameter(A_log)
+        # self.register_buffer("A_log", torch.zeros(self.nheads, dtype=torch.float32, device=device), persistent=True)
         self.A_log._no_weight_decay = True
 
         # D "skip" parameter
-        self.D = nn.Parameter(torch.ones(self.d_ssm if self.D_has_hdim else self.nheads, device=device))
+        self.D = nn.Parameter(
+            torch.ones(
+                self.d_inner if self.D_has_hdim else self.nheads,
+                device=device
+            )
+        )
         self.D._no_weight_decay = True
 
+        # Extra normalization layer right before output projection
         if self.rmsnorm:
             assert RMSNormGated is not None
-            self.norm = RMSNormGated(self.d_ssm, eps=1e-5, norm_before_gate=self.norm_before_gate,
-                                     group_size=self.d_ssm // ngroups, **factory_kwargs)
+            self.norm = RMSNormGated(self.d_inner, eps=1e-5, norm_before_gate=self.norm_before_gate,
+                                     group_size=self.d_inner // ngroups, **factory_kwargs)
 
         if self.process_group is None:
             self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
@@ -129,20 +140,22 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
                                               process_group=self.process_group, sequence_parallel=self.sequence_parallel,
                                               **factory_kwargs)
 
-    def forward(self, u):
+
+    def forward(self, u, seq_idx=None):
         """
-        u: (batch, seqlen, hidden_dim)
+        u: (B, L, D)
         Returns: same shape as u
         """
+        batch, seqlen, dim = u.shape
 
         zxbcdt = self.in_proj(u)  # (B, L, d_in_proj) or (B * L, d_in_proj)
-       
+
         # If the model is loaded in fp16, without the .float() here, A might be -inf
         A = -torch.exp(self.A_log.float())  # (nheads) or (d_inner, d_state)
-    
-        dt_limit_kwargs = {} 
+        dt_limit_kwargs = {} if self.dt_limit == (0.0, float("inf")) else dict(dt_limit=self.dt_limit)
 
         if self.use_mem_eff_path:
+            # Fully fused path
             out = mamba_split_conv1d_scan_combined(
                 zxbcdt,
                 rearrange(self.conv1d.weight, "d 1 w -> d w"),
@@ -168,20 +181,24 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
                 out = reduce_fn(out, self.process_group)
 
         else:
-            d_mlp = (zxbcdt.shape[-1] - 2 * self.d_ssm - 2 * self.ngroups * self.d_state - self.nheads) // 2
-
-            z0, x0, z, xBC, dt = torch.split(
-                zxbcdt,
-                [d_mlp, d_mlp, self.d_ssm, self.d_ssm + 2 * self.ngroups * self.d_state, self.nheads],
+            z, xBC, dt = torch.split(
+                zxbcdt, [self.d_inner, self.d_inner + 2 * self.ngroups * self.d_state, self.nheads], 
                 dim=-1
             )
-            
+
+            # this seems additional to the simple version
+            dt = F.softplus(dt + self.dt_bias)  # (B, L, nheads)
+
             assert self.activation in ["silu", "swish"]
+
+            # 1D Convolution
             xBC = self.act(
                 self.conv1d(xBC.transpose(1, 2)).transpose(1, 2)[:, :-(self.d_conv - 1)]
             )  # (B, L, self.d_ssm + 2 * ngroups * d_state)
 
-            x, B, C = torch.split(xBC, [self.d_ssm, self.ngroups * self.d_state, self.ngroups * self.d_state], dim=-1)
+            # Split into 3 main branches: X, B, C
+            # These correspond to V, K, Q respectively in the SSM/attention duality
+            x, B, C = torch.split(xBC, [self.d_inner, self.ngroups * self.d_state, self.ngroups * self.d_state], dim=-1)
 
             y = mamba_chunk_scan_combined(
                 rearrange(x, "b l (h p) -> b l h p", p=self.headdim),
@@ -195,19 +212,13 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
                 dt_bias=self.dt_bias,
                 dt_softplus=True,
                 seq_idx=None,
-                cu_seqlens=None,
                 **dt_limit_kwargs,
-                return_final_states=False,
-                return_varlen_states=False,
             )
-           
             y = rearrange(y, "b l h p -> b l (h p)")
 
+            # Multiply "gate" branch and apply extra normalization layer
             if self.rmsnorm:
                 y = self.norm(y, z)
-
-            if d_mlp > 0:
-                y = torch.cat([F.silu(z0) * x0, y], dim=-1)
 
             out = self.out_proj(y)
 
